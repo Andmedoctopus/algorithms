e705b8c277e5504c5d96955d7b22a2093b106059
woarewe <rostislav.zhuravsky@gmail.com>
2019-04-15 18:25:22 +0300

    Fix indent and rewrite code using OOP

--- binary_search.py
+++ binary_search.py
@@ -1,19 +1,20 @@
 def binary_search(list, number):
-	low  = 0
-	high = len(list) - 1
-	while low <= high:
-		mid = (high + low) // 2
-		if list[mid] == number:
-			return mid
-		elif list[mid] > number:
-			high = mid - 1
-		elif list[mid] < number:
-			low = mid + 1
-	return None
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if list[mid] == number:
+            return mid
+        elif list[mid] > number:
+            high = mid - 1
+        elif list[mid] < number:
+            low = mid + 1
+    return None
 
 
-def make_list(max):
-	return list(range(1, max))
+def make_list(border):
+    return list(range(1, border))
+
 
 list1 = make_list(10)
 list2 = make_list(15)
@@ -22,3 +23,28 @@ list2[4] = 4.5
 print(list2)
 print(binary_search(list1, 3))
 print(binary_search(list2, 5))
+
+
+# Look at small magic
+class ListWithBinarySearch(list):
+    def binary_search(self, number):
+        return binary_search(self, number)
+
+    @classmethod
+    def list_example(cls, range_border):
+        return ListWithBinarySearch(range(1, range_border))
+
+
+# And now you can write your code so)
+print('Examples with new class')
+
+# Create your own object of new class
+new_list = ListWithBinarySearch([1, 2, 3, 4, 5, 6])
+print(new_list.binary_search(4))
+print(new_list.binary_search(7))
+
+# Or can get random list using class method
+
+random_list = ListWithBinarySearch.list_example(50)
+print(random_list.binary_search(3))
+print(random_list.binary_search(61))
--- selectionsort.py
+++ selectionsort.py
@@ -1,22 +1,26 @@
 import random
 
+
 def find_smallest_index(arr):
-	smallest = arr[0] 
-	smallest_index = 0
-	for i in range(1,len(arr)):
-		if smallest > arr[i]:
-			smallest = arr[i]
-			smallest_index = i
-	return smallest_index
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
 
 def select_sort(arr):
-	newarr = []
-	for i in range(0, len(arr)):
-		newarr.append(arr.pop(find_smallest_index(arr)))
-	return newarr
+    newarr = []
+    for i in range(0, len(arr)):
+        newarr.append(arr.pop(find_smallest_index(arr)))
+    return newarr
 
-our_arr = [3,4,2,1,5,3,2]
-rand_arr = [random.randint(0,100) for i in range(0,10)]
+
+our_arr = [3, 4, 2, 1, 5, 3, 2]
+rand_arr = [random.randint(0, 100) for i in range(0, 10)]
 print('our_arr = {}\nrand_arr = {}'.format(our_arr, rand_arr))
-print('smallest in our_arr = {}\nsmallest in rand_arr = {}'.format(find_smallest_index(our_arr),find_smallest_index(rand_arr)))
-print('sort our_arr = {}, \nsort rand_arr = {}'.format(select_sort(our_arr),select_sort(rand_arr)))
+print('smallest in our_arr = {}\nsmallest in rand_arr = {}'.format(find_smallest_index(our_arr),
+                                                                   find_smallest_index(rand_arr)))
+print('sort our_arr = {}, \nsort rand_arr = {}'.format(select_sort(our_arr), select_sort(rand_arr)))
